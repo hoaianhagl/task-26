5a69c406163168cc076e5d5d96786c7904dfae94
Anh Nguyen Hoai <nguyen_hoai_anh@allgrow-labo.jp>
2022-07-29 17:33:51 +0700

    first commit

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -1,1 +1,13 @@
-console.log("main.js");
+/* eslint-disable linebreak-style */
+
+$(document).ready(function(){
+    $(".p-design__slideshow").slick({
+        slideToSlide: 1,
+        dots: false,
+        infinite: true,
+        cssEase: "linear",
+        arrows: true,
+        prevArrow: "<button type='button' class='slick-prev'></button>",
+        nextArrow:"<button type='button' class='slick-next'></button>",
+    });
+});
